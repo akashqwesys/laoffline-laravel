077e24390ea72186a2958d87a0ced3060f4d23a3
dipesh c <dipeshchangawala@gmail.com>
2025-04-29 23:02:13 +0530

    Add option in header to open change password link

--- resources/views/layouts/includes/header.blade.php
+++ resources/views/layouts/includes/header.blade.php
@@ -101,7 +101,8 @@
               <div class="dropdown-inner">
                 <ul class="link-list">
                   <li><a href="html/user-profile-regular.html"><em class="icon ni ni-user-alt"></em><span>View
-                        Profile</span></a></li>
+                    Profile</span></a></li>
+                  <li><a href="/change-password"><em class="icon ni ni-lock-alt"></em><span>Change Password</span></a></li>
                   {{-- <li><a href="html/user-profile-setting.html"><em class="icon ni ni-setting-alt"></em><span>Account Setting</span></a></li>
                                     <li><a href="html/user-profile-activity.html"><em class="icon ni ni-activity-alt"></em><span>Login Activity</span></a></li> --}}
                   <li><a class="dark-switch" href="#"><em class="icon ni ni-moon"></em><span>Dark Mode</span></a>
